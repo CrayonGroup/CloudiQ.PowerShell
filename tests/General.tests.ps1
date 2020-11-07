5a9a18552dd44635a7962e38d45b9b324f16c8ca
Roberth Strand <me@robstr.dev>
2020-11-07 23:12:57 +0100

    Added tests for Connect-CloudiQ

---
+++ tests/General.tests.ps1
@@ -0,0 +1,15 @@
+BeforeAll {
+    # Import module
+    Import-Module -Name "$PSScriptRoot/../CloudiQ/CloudiQ.psd1" -Force -ErrorAction Stop
+    . "$PSScriptRoot/mocks/general.ps1"
+}
+
+Describe 'tests connecting to the api' {
+    It 'should return the access token' {
+        New-Item -Path 'Env:' -Name 'CloudiQUsername' -Value 'Pester' -Force
+        New-Item -Path 'Env:' -Name 'CloudiQPassword' -Value 'PesterPass' -Force
+        Connect-CloudiQ
+        
+        $CloudIqAccessToken | Should -Be 'AccessToken'
+    }
+}
