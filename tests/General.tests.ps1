8d4034133133413f3f999cff12bb4a5a41a4a18b
Roberth Strand <me@robstr.dev>
2020-11-16 16:38:42 +0100

    Added test for bad requests

--- tests/General.tests.ps1
+++ tests/General.tests.ps1
@@ -8,8 +8,15 @@ Describe 'tests connecting to the api' {
     It 'should return the access token' {
         New-Item -Path 'Env:' -Name 'CloudiQUsername' -Value 'Pester' -Force
         New-Item -Path 'Env:' -Name 'CloudiQPassword' -Value 'PesterPass' -Force
+
+        New-Item -Path 'Env:' -Name 'CloudiQClientId' -Value 'PesterClientId' -Force
+        New-Item -Path 'Env:' -Name 'CloudiQClientSecret' -Value 'PesterClientSecret' -Force
         Connect-CloudiQ | Out-Null
         
         $CloudIqAccessToken | Should -Be 'AccessToken'
+    }
+
+    it 'should fail to connect' {
+        { Connect-CloudiQ -ClientId 'BadRequest' -ClientSecret 'BadRequest' -ErrorAction Stop } | Should -Throw
     }
 }
